307967c835c972f64161e98ae7e5593f85874b14
Alexander Weinrauch <alexander.weinrauch@gmail.com>
2024-01-05 09:15:28 +0100

    Initial commit

---
+++ lua/plugins/core.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'tokyonight'
+    end,
+  },
+  {
+    'NMAC427/guess-indent.nvim',
+    opts = {}
+  },
+  {
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').setup{}
+      require('which-key').register {
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = 'Git[H]unk', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      }
+      require('which-key').register({
+        ['<leader>'] = { name = 'VISUAL <leader>' },
+        ['<leader>h'] = { 'Git [H]unk' },
+      }, { mode = 'v' })
+    end,
+  },
+}
---
+++ lua/plugins/editor.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    opts = {},
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {},
+  },
+  { 'numToStr/Comment.nvim', opts = {} },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = { 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'cmake', 'markdown', 'markdown_inline' },
+        sync_install = false,
+        indent = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+          -- disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end,
+  },
+}
