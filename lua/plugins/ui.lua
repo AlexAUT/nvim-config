3b56d427addcfea0813de182eb29d93c4928b204
Alexander Weinrauch <alexander.weinrauch@tugraz.at>
2024-01-19 11:48:02 +0100

    Added barbecue

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,4 +3,15 @@ return {
     'stevearc/dressing.nvim',
     opts = {},
   },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+    },
+  }
 }
